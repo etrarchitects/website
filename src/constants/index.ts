e9a4d6af6ab5b89d27b8103f2ee1714fd65bab71
dagyu95 <dagyu95@gmail.com>
2021-05-20 15:49:07 +0200

    Add environment variable REACT_APP_IS_PREVIEW

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
 import { PublicationState } from "../generated/globalTypes";
 
 export const apiUrl = process.env.REACT_APP_STRAPI_API;
-export const publicationState = process.env.NODE_ENV === "production" || process.env.NODE_ENV === "test" ? PublicationState.LIVE : PublicationState.PREVIEW
+export const publicationState = String(process.env.REACT_APP_IS_PREVIEW).toLocaleLowerCase() === "true" ? PublicationState.PREVIEW : PublicationState.LIVE
